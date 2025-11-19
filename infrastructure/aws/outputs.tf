60d09d0c784dffa448956982f0f03ee8213ade86
nfOlguin <naamanalejandrofalcon@gmail.com>
2025-11-19 20:35:26 -0300

    Delete infrastructure directory

--- infrastructure/aws/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "bucket_name" {
-  value = aws_s3_bucket.static.bucket
-}
-
-output "rds_endpoint" {
-  value = aws_db_instance.rds.endpoint
-}
--- infrastructure/aws/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "region" {
-  default = "us-east-1"
-}
-
-variable "bucket_name" {}
-
-variable "db_username" {}
-
-variable "db_password" {}
